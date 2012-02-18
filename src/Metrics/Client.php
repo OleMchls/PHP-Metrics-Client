5354eedb7238eb44c932c89e6533423da565dc23
Ole Michaelis <Ole.Michaelis@googlemail.com>
2012-02-18 13:30:40 +0100

    First working version

--- src/Metrics/Client.php
+++ src/Metrics/Client.php
@@ -2,6 +2,10 @@
 
 namespace Metrics;
 
+use Buzz\Message\Request;
+use Buzz\Message\Response;
+use Buzz\Client\Curl;
+
 /**
  * Description of Client
  *
@@ -9,8 +13,45 @@ namespace Metrics;
  */
 class Client {
 
-	public function say($oi) {
-		return $oi;
+	protected $email;
+	protected $token;
+
+	const URI = 'https://metrics-api.librato.com';
+	const API_VERSION = 'v1';
+
+	function __construct($email, $token) {
+		$this->email = $email;
+		$this->token = $token;
+	}
+
+	protected function request($path, $method, array $data = array()) {
+		$request = new Request($method, $this->buildPath($path), self::URI);
+		$request->addHeader('Authorization: Basic '.base64_encode($this->email . ':' . $this->token));
+		$request->addHeader('Content-Type: application/json');
+		$request->setContent(json_encode($data));
+		$response = new Response();
+
+		$client = new Curl();
+		$client->send($request, $response);
+
+		return json_decode($response->getContent());
+	}
+
+	protected function buildPath($path) {
+		return '/' . self::API_VERSION . $path;
+	}
+
+	protected function buildUri($email, $token, $uri) {
+		$auth = urlencode($email . '@' . $token);
+		return sprintf($uri, $auth);
+	}
+
+	public function get($path) {
+		return $this->request($path, Request::METHOD_GET);
+	}
+
+	public function post($path, array $data) {
+		return $this->request($path, Request::METHOD_POST, $data);
 	}
 
 }
--- test/Metrics/ClientTest.php
+++ test/Metrics/ClientTest.php
@@ -9,10 +9,31 @@ use Metrics\Client;
  */
 class ClientTest extends \PHPUnit_Framework_TestCase {
 
-	public function testSay() {
-		$client = new Client();
-		$var = 'string';
-		$this->assertEquals($var, $client->say($var));
+	protected $data = array(
+		'gauges' => array(
+			array(
+				'name' => 'php-librato-metrics-test',
+				'value' => 123
+			)
+		)
+	);
+
+	protected function buildClient() {
+		return new Client($_ENV['metrics_email'], $_ENV['metrics_token']);
+	}
+
+	public function testPostingDataT0Metrics() {
+		$client = $this->buildClient();
+		$response = $client->post('/metrics', $this->data);
+		$this->assertNull($response);
+	}
+
+	public function testGettingDataFromMetrics() {
+		$client = $this->buildClient();
+		$client->post('/metrics', $this->data);
+		$metrics = $this->data['gauges'][0];
+		$response = $client->get('/metrics/' . $metrics['name']);
+		$this->assertEquals($response->name, $metrics['name']);
 	}
 
 }
