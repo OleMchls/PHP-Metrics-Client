4758b3b67d0ded0fba10c74ca0df6e1ffcec9677
Ole Michaelis <Ole.Michaelis@googlemail.com>
2012-02-19 09:02:01 +0100

    improved unittests

--- test/Metrics/ClientTest.php
+++ test/Metrics/ClientTest.php
@@ -3,37 +3,59 @@
 use Metrics\Client;
 
 /**
- * Description of ClientTest
+ * Tests for Metrics Client.
  *
- * @author ole
+ * @author Ole 'nesQuick' Michaelis <Ole.Michaelis@googlemail.com>
  */
 class ClientTest extends \PHPUnit_Framework_TestCase {
-
-	protected $data = array(
-		'gauges' => array(
-			array(
-				'name' => 'php-librato-metrics-test',
-				'value' => 123
-			)
-		)
-	);
 
 	protected function buildClient() {
 		return new Client($_ENV['metrics_email'], $_ENV['metrics_token']);
 	}
 
-	public function testPostingDataT0Metrics() {
+	public function gaugesProvider() {
+		return array(
+			array(// 1. Run
+				array(// 1. Param
+					'gauges' => array(
+						array(
+							'name' => 'php-librato-metrics-test',
+							'value' => mt_rand(100, 1337)
+						)
+					)
+				)
+			),
+			array(// 2. Run
+				array(// 1. Param
+					'gauges' => array(
+						array(
+							'name' => 'php-librato-metrics-test',
+							'value' => mt_rand(100, 1337)
+						)
+					)
+				)
+			)
+		);
+	}
+
+	/**
+	 * @dataProvider gaugesProvider
+	 */
+	public function testPostingDataT0Metrics($metrics) {
 		$client = $this->buildClient();
-		$response = $client->post('/metrics', $this->data);
+		$response = $client->post('/metrics', $metrics);
 		$this->assertNull($response);
 	}
 
-	public function testGettingDataFromMetrics() {
+	/**
+	 * @dataProvider gaugesProvider
+	 */
+	public function testGettingDataFromMetrics($metrics) {
 		$client = $this->buildClient();
-		$client->post('/metrics', $this->data);
-		$metrics = $this->data['gauges'][0];
-		$response = $client->get('/metrics/' . $metrics['name']);
-		$this->assertEquals($response->name, $metrics['name']);
+		$client->post('/metrics', $metrics);
+		$metric = $metrics['gauges'][0];
+		$response = $client->get('/metrics/' . $metric['name']);
+		$this->assertEquals($response->name, $metric['name']);
 	}
 
 }
